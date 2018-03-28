1d3ecc43da0dafbe49fa1d7e52d918a558d54df5
balintataw <balintataw2@gmail.com>
2018-03-28 14:54:34 -0700

    initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,54 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Link } from 'react-router-dom';
 import './App.css';
-import axios from 'axios'
+import axios from 'axios';
+import 'font-awesome/css/font-awesome.min.css';
 
 class App extends Component {
+  state = {
+    fieldData: [],
+  }
   componentDidMount() {
     axios.get('http://localhost:3001/fields').then(resp => {
       console.log(resp.data)
+      this.setState({
+        fieldData: resp.data
+      })
+     
     })
+  }
+
+  handleFormSubmit() {
+
   }
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <div className="App">
+          <div className="header">
+            <h1>File a claim</h1>
+          </div>
+          <form onSubmit={this.handleFormSubmit} id="form-container">
+              {this.state.fieldData.map((field, i) => {
+                if(field.type === "text" || field.type === "email" || field.type === "tel") {
+                  return <div key={"kevin" + i}><i className={"fa " + field.icon}></i>
+                  <input  type={field.type} className="inputs" label={field.label} id={field.id} placeholder={field.label}/></div>
+                
+                } else if (field.type === "select") {
+                  return <select key={"kevin" + i} type={field.type} className="select" label={field.label} id={field.id}>
+                          {field.options.map((option, i) => {
+                            return <option key={"kevin" + i}label={option.label}>{option.value}</option>
+                          })}
+                        </select>
+                } else if (field.type === "textarea") {
+                  return <div key={"kevin" + i}><i className={"fa " + field.icon} ></i>
+                  <textarea type={field.type} className="textarea" label={field.label} id={field.id}></textarea></div>
+                } 
+              })}
+              <button type="button" id="btn1">Get Lost</button>
+          </form>
+        </div>
       </div>
     );
   }
