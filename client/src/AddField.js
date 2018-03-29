f80bb143b0df454731084bcc5f5afc5d8b404908
balintataw <balintataw2@gmail.com>
2018-03-28 22:57:10 -0700

    new form data is added to db.json / though not in correct format because pushing all of this.state

--- client/src/AddField.js
+++ client/src/AddField.js
@@ -2,16 +2,24 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Link } from 'react-router-dom';
 // import App from './App';
 import './AddField.css';
+import axios from 'axios';
 
 export class AddField extends Component {
   state = {
       selectValue: 'text',
       options: [],
-      isHidden: true,
-      optionValue: ''
+      isDisabled: true,
+      optionValue: '',
+      label: '',
+      id: '',
+      icon: '',
   }
-  handleFormSubmit() {
-
+  handleFormSubmit = (e) => {
+      e.preventDefault()
+    console.log(this.state)
+    axios.post('http://localhost:3001/fields', this.state).then(resp => {
+        this.props.history.push('/AddField')
+    }).catch(e => console.log(e)) 
   }
 
   handleSelectChange = (e) => {
@@ -21,59 +29,95 @@ export class AddField extends Component {
     })
     if (value === 'Select') {
         console.log('worked')
-        // this.toggleHidden()
+        this.toggleDisabled()
 
     }
   }
 
-  handleOptionChange = (e) => {
+  handleChange = (e) => {
       const value = e.target.value
-      console.log(value)
       this.setState({
           [e.target.name]: value
       })
-      console.log(this.state.options)
   }
 
   addToOptions = (e) => {
-      e.preventDefault()
+    e.preventDefault()
     this.state.options.push(this.state.optionValue)
     this.state.optionValue = ''
-    console.log(this.state.options)
   } 
 
-//   toggleHidden = (e) => {
-//       this.setState({
-//           isHidden: !this.state.isHidden
-//       })
-//   }
+  toggleDisabled = (e) => {
+      this.setState({
+          isDisabled: !this.state.isDisabled
+      })
+  }
 
   render() {
     return (
       <div>
           <form onSubmit={this.handleFormSubmit} id="form-container">
              <div>
-               <select onChange={this.handleSelectChange} value={this.state.selectValue} type="select" className="inputs" label="Field Type" id="field-type" placeholder="Field Type" name="selectValue">
+                {/* select and options fields */}
+               <select onChange={this.handleSelectChange} 
+                        value={this.state.selectValue} 
+                        type="select" 
+                        className="inputs" 
+                        label="Field Type" 
+                        id="field-type" 
+                        placeholder="Field Type" 
+                        name="selectValue">
                 <option value="txt">Text</option>
                 <option>Select</option>
                 <option>Text Area</option>
                </select>
-               <input onChange={this.handleOptionChange} value={this.state.optionValue} type="text" label="Field Option" id="field-option" placeholder="Option" name="optionValue"/>
+               <input onChange={this.handleChange} 
+                        onClick={this.toggleDisabled} 
+                        value={this.state.optionValue} 
+                        disabled={(this.state.disabled)? "disabled" : ""}
+                        type="text" 
+                        label="Field Option" 
+                        id="field-option" 
+                        placeholder="'key':value" 
+                        name="optionValue"/>
                <button onClick={this.addToOptions} id="addOption">+</button>
-               {/* {!this.state.isHidden && <Child optionValue={this.state.optionValue}/>} */}
              </div>
+             {/* field label */}
              <div>
-               <input type="text" className="inputs" label="Field Label" id="field-label" placeholder="Field Label"/>
+               <input onChange={this.handleChange}
+                        value={this.state.label} 
+                        type="text" 
+                        className="inputs" 
+                        label="Field Label" 
+                        id="field-label" 
+                        placeholder="Field Label" 
+                        name="label"/>
              </div>
+             {/* field id */}
              <div>
-               <input type="text" className="inputs" label="Field ID" id="field-id" placeholder="Field ID"/>
+               <input onChange={this.handleChange}
+                        value={this.state.id} 
+                        type="text" 
+                        className="inputs" 
+                        label="Field ID" 
+                        id="field-id" 
+                        placeholder="Field ID" 
+                        name="id"/>
              </div>
+             {/* field icon */}
              <div>
-               <input type="text" className="inputs" label="Field icen" id="field-icon" placeholder="Field Icon"/>
+               <input onChange={this.handleChange}
+                        value={this.state.icon} 
+                        type="text" 
+                        className="inputs" 
+                        label="Field icen" 
+                        id="field-icon" 
+                        placeholder="Field Icon" 
+                        name="icon"/>
              </div>
               <div className="buttons">
-                <Link to="/"><button type="button" id="btn1">Get Lost</button></Link>
-                <Link to='/AddField'><button type="button" id="btn2">Add Field</button></Link>
+                <Link to='/ShowField'><button type="button" id="btn3">Go Back</button></Link>
+                <button  type="submit" id="btn4">Add Field</button>
               </div>
           </form>
       </div>
